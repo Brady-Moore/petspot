5dfe36d8ff7add604cc12cabd233969944861648
pedro sassaki <pedropq2001@gmail.com>
2025-08-26 18:03:11 +0900

    created amenity.scss changed amenity  and location model show html not finished yet

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,2 +1,17 @@
 class Amenity < ApplicationRecord
+  has_many :location_amenities
+  has_many :locations, through: :location_amenities
+
+  def icon_class
+    case key.downcase
+    when "water_bowls"
+      "fas fa-faucet-drip"
+    # when ""
+    #   "fa-solid "
+    # when ""
+    #   "fa-solid "
+    else
+      "fa-solid fa-circle-question"
+    end
+  end
 end
