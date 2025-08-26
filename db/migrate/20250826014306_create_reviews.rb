c4236f03ebe3b52e5d36d5188008f68ae8741dfb
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-08-26 11:39:58 +0900

    added models

---
+++ db/migrate/20250826014306_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reviews do |t|
+      t.references :location, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.integer :rating
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
