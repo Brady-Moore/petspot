357305c25e654efb5b7c035ff078f7f7758a22f1
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-08-27 15:47:48 +0900

    Favorites index (#17)
    * Working favorites
    
    * cards and centered h1
    
    * changed card-trip img css selector
    
    * favorites turbo stream delete

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,20 @@
 class FavoritesController < ApplicationController
 
   def index
+    @favorites = current_user.favorited_by_type("Location") || []
   end
 
   def create
   end
 
   def destroy
+    @location = Location.find(params[:id])
+    current_user.unfavorite(@location)
+    @favorites = current_user.favorited_by_type("Location") || []
+
+    respond_to do |format|
+      format.turbo_stream # renders destroy.turbo_stream.erb
+      format.html { redirect_to favorites_path, notice: "#{@location.name} removed." }
+    end
   end
 end
