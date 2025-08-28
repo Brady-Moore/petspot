c7318994e2ab444372ad0c66e157837da9198452
Guilherme Kenji <kenjieto99@gmail.com>
2025-08-28 19:04:30 +0900

    home page padding-bottom fixed and exapandable filters added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   def home
     @amenities = Amenity.all
-    @first_half = @amenities[0..(@amenities.count/2)]
-    @second_half = @amenities - @first_half
+    @groups = @amenities.in_groups(4, false)
   end
 end
