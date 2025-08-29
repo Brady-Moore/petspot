047eecc6555d5052b3a041aa7590c896f8ecd9bb
Hyelim Kim <real7210@gmail.com>
2025-08-29 09:53:13 +0900

    filter-updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+
   def home
-    @amenities = Amenity.all
-    @groups = @amenities.in_groups(4, false)
   end
 end
