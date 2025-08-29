590bdd1f3ab2b08b5a086cf50edd6c924634cb14
Guilherme Kenji <kenjieto99@gmail.com>
2025-08-29 09:48:16 +0900

    filtered results working (#33)
    * filtered results working
    
    * combine two tables to make find results faster
    
    * filter results working
    
    * 0 results
    
    * Displaying results message with number of the results
    
    * home page padding-bottom fixed and exapandable filters added
    
    * everything works perfectly and indententation fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   def home
+    @amenities = Amenity.all
+    @groups = @amenities.in_groups(4, false)
   end
 end
