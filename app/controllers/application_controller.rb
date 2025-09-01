227f1a1f6dde4fd0ad928e9410b6f8d1a28f27a4
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-09-01 13:01:42 +0900

    name submission fix and edit functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
-
   before_action :authenticate_user!
   before_action :set_amenities
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # For sign up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For account update (Devise edit profile)
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
 
   def set_amenities
     @amenities = Amenity.all
